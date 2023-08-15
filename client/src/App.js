7d837aad114f5f4804d221f8592928b93f999379
BhaveshG-22 <107833962+BhaveshG-22@users.noreply.github.com>
2023-08-15 17:53:00 -0400

    Implement basic chat functionality using Socket.io

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { Home } from "./pages/home";
+
+function App() {
+  return (
+    <div
+      className="App d-flex justify-content-center align-items-center   container"
+      style={{ height: "100vh" }}
+    >
+      <Home />
+    </div>
+  );
+}
+
+export default App;
